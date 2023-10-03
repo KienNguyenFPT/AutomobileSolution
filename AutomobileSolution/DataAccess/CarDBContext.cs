5b1f108a7a97d4f96afdf5d0eceb6a44e12bb0fd
TRUNG KIEN <TRUNG KIEN@LAPTOP-V3HIBDP6>
2023-10-03 08:22:54 +0700

    Add project files.

---
+++ AutomobileSolution/DataAccess/CarDBContext.cs
@@ -0,0 +1,44 @@
+﻿using AutomobileSolution.BusinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomobileSolution.DataAccess
+{
+    public class CarDBContext
+    {
+        private static List<Car> CarList = new List<Car>()
+        {
+            new Car
+            {
+                CarID = 1,
+                CarName = "LV",
+                Manufacturer = "Honda",
+                Price = 30000,
+                ReleaseYear = 2021
+            },
+            new Car
+            {
+                CarID = 2,
+                CarName = "Yamaha",
+                Manufacturer = "Ford",
+                Price = 3000000,
+                ReleaseYear = 2020
+
+            }
+        };
+
+        private static CarDBContext instance = null;
+        public static readonly object instanceLook = new object();
+        private CarDBContext() { }
+        public static CarDBContext Instance
+        {
+            get
+            {
+
+            }
+        }
+    }
+}
---
+++ AutomobileSolution/Repository/CarRepository.cs
@@ -0,0 +1,37 @@
+﻿using AutomobileSolution.BusinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomobileSolution.Repository
+{
+    internal class CarRepository : ICarRepository
+    {
+        public void DeleteCar(int carId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car GetCarByID(int carId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Car> GetCars()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertCar(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCar(Car car)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
